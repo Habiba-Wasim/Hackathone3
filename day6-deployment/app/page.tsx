4ae4e52b2943dc7f978dd2c7d5743be3e6a20396
Habiba <160347457+Habiba-Wasim@users.noreply.github.com>
2025-02-07 20:11:32 +0500

    Add files via upload

---
+++ day6-deployment/app/page.tsx
@@ -0,0 +1,23 @@
+
+
+import Card from "@/components/Card";
+import Essentials from "@/components/Essentials";
+import Featured from "@/components/Featured";
+import Gear from "@/components/Gear";
+import HeroSection from "@/components/HeroSection";
+import Stan from "@/components/Stan";
+import { ChevronLeft } from "lucide-react";
+
+
+export default function Home() {
+  return (
+    <>
+ <HeroSection/>
+ <Card/>
+ <Featured/>
+ <Gear/>
+ <Stan/>
+ <Essentials/>
+ </>
+  );
+}
